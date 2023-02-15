557c3eaf9c3053c1703e0150891eae477d45762b
atteasi <atte.asikainen@metropolia.fi>
2023-02-15 10:48:25 +0200

    Done

---
+++ src/test/java/ExtraTest.java
@@ -0,0 +1,70 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import laskin.Laskin;
+
+public class ExtraTest extends AbstractParent {
+
+    private static Laskin laskin = new Laskin();
+    private final double DELTA = 0.001;
+
+    @BeforeAll
+    public static void testVirtaON() {
+        System.out.println("@BeforeAll Virta ON (ennen ensimmäistä testiä)");
+        laskin.virtaON();
+    }
+
+    @AfterAll
+    public static void testVirtaOFF() {
+        System.out.println("@AfterAll Virta OFF (kaikki testit ajettu).");
+        laskin.virtaOFF();
+        laskin = null;
+    }
+
+    @BeforeEach
+    public void testNollaa() {
+        System.out.println("  Nollaa laskin.");
+        laskin.nollaa();
+        assertEquals(0, laskin.annaTulos(), "Nollaus ei onnistunut");
+    }
+
+    @Test
+    public void testNelio2() {
+        laskin.nelio(2);
+        assertEquals(4, laskin.annaTulos(), "Luvun 2 Neliöön korotus väärin");
+    }
+
+    @Test
+    public void testNelio4() {
+        laskin.nelio(4);
+        assertEquals(16, laskin.annaTulos(),"Luvun 4 neliöön korotus väärin");
+    }
+
+    @Test
+    public void testNelio5() {
+        laskin.nelio(5);
+        assertEquals(25, laskin.annaTulos(), DELTA,"Luvun 5 neliöön korotus väärin");
+    }
+    @Test
+    public void testNeliojuuri2() {
+        laskin.neliojuuri(2);
+		assertEquals(Math.sqrt(2), laskin.annaTulos(), DELTA,"Luvun 2 neliöjuuren saanti väärin");
+    }
+    
+    @Test
+    @DisplayName("Testaa negatiivinen neliöjuuri")
+    public void testNeliojuuriNegat() {
+        ArithmeticException poikkeus = assertThrows(ArithmeticException.class, () -> laskin.neliojuuri(-1),
+                "Negatiivinen neliöjuuri ei tuottanut poikkeusta");
+        assertEquals("Negatiivisesta luvusta ei voi otta neliöjuurta", poikkeus.getMessage(), "Väärä virheilmoitus");
+    }
+}
